084b0da1343de1a702e496403da70abf3ff41b35
Meathill <meathill@gmail.com>
2021-03-13 22:00:09 +0800

    change: changed main UI

--- src/app.vue
+++ src/app.vue
@@ -1,106 +1,107 @@
 <template lang="pug">
-#app
-  nav.p-3.bg-blue-500.text-white
-    .navbar-nav Not Final Cut
+nav.bg-blue-500.text-white.flex.py-2.items-center.px-3
+  .navbar-brand.text-xl
+    strong N
+    small.text-blue-200 ot
+    strong.ml-1 F
+    small.text-blue-200 inal
+    strong.ml-1 C
+    small.text-blue-200 ut
 
-  .container.mx-auto.py-5(v-if="isLoading")
-    .bi.bi-spin.text-7xl.mx-auto
+  .mx-auto
+    strong {{title}}
 
-  .container.mx-auto.flex.pt-4(v-else)
-    .video(class="w-1/2")
-      h2.text-lg.text-bold.pl-3.py-4.border-2.border-gray-200.rounded-t-md.border-b-0 视频预览
-      video.w-full.mb-3(
-        :src="movieSrc",
-        controls,
-      )
+  transition(
+    name="alert-transition",
+    enter-active-class="animated slideInDown",
+    leave-active-class="animated slideOutUp",
+  )
+    .alert.py-2.px-3.m-3.rounded-md.mr-3(
+      v-if="message",
+      :class="status === true ? 'bg-green-300' : 'bg-red-300'",
+    ) {{message}}
 
-      h1.text-lg.text-bold.mb-2 {{title}}
-      p.mb-2 创建时间：
-        time.text-gray-500(:datetime="createdAt") {{createdAt}}
-      p.mb-4 更新时间：
-        time.text-gray-500(:datetime="updatedAt") {{updatedAt}}
+  .action-bar.flex.text-sm
+    button.px-3.rounded.bg-green-500.leading-9.transition(
+      class="hover:bg-green-600",
+      type="button",
+      @click="doSave",
+      :disabled="isSaving",
+    )
+      i.bi.bi-spin.animate-spin.mr-2(v-if="isSaving")
+      i.bi.bi-check.mr-2(v-else)
+      | 保存项目 (
+      span.underline S
+      | )
 
-      transition(
-        name="alert-transition",
-        enter-active-class="animated slideInDown",
-        leave-active-class="animated slideOutUp",
-      )
-        .alert.text-white.py-2.px-3.rounded-md(
-          v-if="message",
-          :class="status === true ? 'bg-green-300' : 'bg-red-300'",
-        ) {{message}}
+    button.px-3.rounded.bg-blue-600.ml-2.leading-9.transition(
+      class="hover:bg-blue-700",
+      type="button",
+      @click="doExport",
+      :disabled="isExporting",
+    )
+      i.bi.bi-spin.animate-spin.mr-2(v-if="isExporting")
+      i.bi.bi-file-earmark-play-fill.mr-2(v-else)
+      | 导出视频 (
+      span.underline E
+      | )
 
-      .action-bar.mt-2.flex
-        button.px-3.py-2.rounded-md.border-2.border-green-400.bg-green-500.text-white(
-          type="button",
-          @click="doSave",
-          :disabled="isSaving",
-        )
-          i.bi.bi-spin.animate-spin.mr-2(v-if="isSaving")
-          i.bi.bi-check.mr-2(v-else)
-          | 保存项目 (
-          span.underline S
-          | )
+    button.px-3.rounded.bg-indigo-500.ml-2.leading-9.transition(
+      class="hover:bg-indigo-600",
+      type="button",
+      @click="doExportSubtitles",
+      :disabled="isExportingSubtitles",
+    )
+      i.bi.bi-spin.animate-spin.mr-2(v-if="isExportingSubtitles")
+      i.bi.bi-file-earmark-font-fill.mr-2(v-else)
+      | 导出字幕 (
+      span.underline T
+      | )
 
-        button.px-3.py-2.rounded-md.border-2.border-blue-400.bg-blue-500.text-white.ml-3(
-          type="button",
-          @click="doExport",
-          :disabled="isExporting",
-        )
-          i.bi.bi-spin.animate-spin.mr-2(v-if="isExporting")
-          i.bi.bi-file-earmark-play-fill.mr-2(v-else)
-          | 导出视频 (
-          span.underline E
-          | )
+.bi.bi-spin.text-7xl.mx-auto.my-5.mx-auto(v-if="isLoading")
 
-        button.w-32.py-2.rounded-md.border-2.border-indigo-400.bg-indigo-500.text-white.ml-3(
-          type="button",
-          @click="doExportSubtitles",
-          :disabled="isExportingSubtitles",
-        )
-          i.bi.bi-spin.animate-spin.mr-2(v-if="isExportingSubtitles")
-          i.bi.bi-file-earmark-font-fill.mr-2(v-else)
-          | 导出字幕 (
-          span.underline T
-          | )
+#main-content.flex.flex-1(v-else)
+  .bg-black.flex-1.flex
+    video.w-full(
+      :src="movieSrc",
+      controls,
+    )
 
-
-    .flex-1.ml-3.border-2.border-gray-200.rounded-md.relative
-      header.flex.items-center.px-3
-        h2.text-lg.text-bold.py-4 调整字幕
-        .help.ml-auto.text-gray-500.text-sm 点击编辑，右键隐藏
-      table.table.w-full
-        thead
-          tr
-            th.bg-red-300.text-white.leading-loose.px-3.border-b-2.border-white.border-r-2(
-              class="w-1/3",
-            ) 时间
-            th.bg-pink-300.text-white.px-3.border-b-2.border-white 字幕
-        tbody
-          tr(v-for="item in subtitles")
-            td.bg-red-50.leading-relaxed.px-3.py-2.border-b-2.border-r-2.border-white
-              small.text-gray-400 开始：
-              strong {{item.start}}
-              br
-              small.text-gray-400 持续：
-              strong.text-green-500.text-sm {{item.duration}}
-            td.bg-pink-50.px-3.py-2.border-b-2.border-white
-              template(v-for="word in item.words")
-                span.px-1.cursor-pointer.border-2.border-transparent(
-                  class="hover:border-red-400",
-                  :class="[{'line-through text-gray-300': word.off}, word.isEdit ? 'hidden' : 'inline-block']",
-                  @click="doEdit(word, $event)",
-                  @contextmenu.prevent="doToggle(word)",
-                ) {{word.text || word.Word}}
-                input.rounded-md.border.px-1.inline-block.mx-1.subtitle-input(
-                  v-if="word.isEdit",
-                  v-model="word.text",
-                  @blur="doAcceptWord(word)",
-                  @keydown.esc="doCancelEdit(word)",
-                  @keydown.enter="doAcceptWord(word)",
-                  :style="'width:' + word.Word.length + 'em'",
-                )
-
+  .w-96.relative.overflow-auto
+    header.flex.items-center.px-3
+      h2.text-lg.text-bold.py-4 调整字幕
+      .help.ml-auto.text-gray-500.text-sm 点击编辑，右键隐藏
+    table.table.w-full
+      thead
+        tr
+          th.bg-red-300.text-white.leading-loose.px-3.border-b-2.border-white.border-r-2(
+            class="w-1/3",
+          ) 时间
+          th.bg-pink-300.text-white.px-3.border-b-2.border-white 字幕
+      tbody
+        tr(v-for="item in subtitles")
+          td.bg-red-50.leading-relaxed.px-3.py-2.border-b-2.border-r-2.border-white
+            small.text-gray-400 开始：
+            strong {{item.start}}
+            br
+            small.text-gray-400 持续：
+            strong.text-green-500.text-sm {{item.duration}}
+          td.bg-pink-50.px-3.py-2.border-b-2.border-white
+            template(v-for="word in item.words")
+              span.px-1.cursor-pointer.border-2.border-transparent(
+                class="hover:border-red-400",
+                :class="[{'line-through text-gray-300': word.off}, word.isEdit ? 'hidden' : 'inline-block']",
+                @click="doEdit(word, $event)",
+                @contextmenu.prevent="doToggle(word)",
+              ) {{word.text || word.Word}}
+              input.rounded-md.border.px-1.inline-block.mx-1.subtitle-input(
+                v-if="word.isEdit",
+                v-model="word.text",
+                @blur="doAcceptWord(word)",
+                @keydown.esc="doCancelEdit(word)",
+                @keydown.enter="doAcceptWord(word)",
+                :style="'width:' + word.Word.length + 'em'",
+              )
 </template>
 
 <script>
